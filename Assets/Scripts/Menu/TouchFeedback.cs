a31cd431eca4bac265720e3e1bf761116b94442f
FacuCesare <facundo.cesare@outlook.es>
2025-06-20 12:01:57 -0300

    Sonido botones

--- Assets/Scripts/Menu/TouchFeedback.cs
+++ Assets/Scripts/Menu/TouchFeedback.cs
@@ -8,14 +8,32 @@ public class TouchFeedback : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     public float scaleFactor = 0.9f;
     private Vector3 originalScale;
 
+    [Header("Audio")]
+    public AudioClip clickSound;
+    private AudioSource audioSource;
+
     void Start()
     {
         originalScale = transform.localScale;
+
+       
+        audioSource = GetComponent<AudioSource>();
+        if (audioSource == null)
+        {
+         
+            audioSource = gameObject.AddComponent<AudioSource>();
+            audioSource.playOnAwake = false;
+        }
     }
 
     public void OnPointerDown(PointerEventData eventData)
     {
         transform.localScale = originalScale * scaleFactor;
+
+        if (clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
     }
 
     public void OnPointerUp(PointerEventData eventData)
